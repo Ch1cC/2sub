6dbf50f87886b9398a5e286dc2fd50f5f09c062b
Ch1cC Chen <48575759+Ch1cC@users.noreply.github.com>
2023-04-19 00:32:36 +0800

    add:先删除sub文件夹

--- 2sub.go
+++ 2sub.go
@@ -60,7 +60,8 @@ func main() {
 	}*/
 	nodes := formatNodes()
 	users := formatUser()
-	os.Mkdir("sub", 0644)
+	os.RemoveAll("sub")
+	os.Mkdir("sub", 0555)
 	//循环vmess,user对象
 	for _, user := range users {
 		//字符串拼接
@@ -163,4 +164,21 @@ func readJSON(path string) ([]byte, error) {
 	open, _ := os.Open(path)
 	defer open.Close()
 	return io.ReadAll(open)
+}
+
+//判断文件文件夹是否存在
+func isFileExist(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	//我这里判断了如果是0也算不存在
+	if fileInfo.Size() == 0 {
+		return false, nil
+	}
+	if err == nil {
+		return true, nil
+	}
+	return false, err
 }
